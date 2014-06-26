c1d4f3a7847dcd24a8741a232eb14b432eb33b7b
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2014-06-26 16:44:17 +0100

    Added changelog to the long description of the distribution

--- setup.py
+++ setup.py
@@ -20,22 +20,30 @@ from setuptools import find_packages
 from setuptools import setup
 
 _CURRENT_DIR_PATH = os.path.abspath(os.path.dirname(__file__))
-_README_CONTENTS = open(os.path.join(_CURRENT_DIR_PATH, 'README.md')).read()
+
+_README_CONTENTS = open(os.path.join(_CURRENT_DIR_PATH, 'README.rst')).read()
+_CHANGELOG_PATH = \
+    os.path.join(_CURRENT_DIR_PATH, 'docs', 'source', 'changelog.rst')
+_CHANGELOG_CONTENTS = open(_CHANGELOG_PATH).read()
+
 _VERSION = \
     open(os.path.join(_CURRENT_DIR_PATH, 'VERSION.txt')).readline().rstrip()
+
+_LONG_DESCRIPTION = _README_CONTENTS + '\n' + _CHANGELOG_CONTENTS
 
 
 setup(
     name='hubspot-contacts',
     version=_VERSION,
-    description='',
-    long_description=_README_CONTENTS,
+    description='Pythonic wrapper for HubSpot API methods in the Contacts, '
+        'Contact Lists and Contact Properties APIs',
+    long_description=_LONG_DESCRIPTION,
     classifiers=[
         ],
-    keywords='',
+    keywords='hubspot contacts lists properties',
     author='2degrees Limited',
     author_email='2degrees-floss@googlegroups.com',
-    url='',
+    url='http://pythonhosted.org/hubspot-contacts/',
     license='BSD (http://dev.2degreesnetwork.com/p/2degrees-license.html)',
     packages=find_packages(exclude=['tests']),
     namespace_packages=['hubspot'],
